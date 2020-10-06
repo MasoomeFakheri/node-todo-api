9367fa722fec60225d7cba44d46fcfba8b7f8024
masi <m.fakheri2009@gmail.com>
2020-10-06 15:36:28 +0330

    TestAPI

---
+++ playground/hashing.js
@@ -0,0 +1,55 @@
+const {SHA256} = require('crypto-js')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+
+
+var password = "123abc!";
+
+bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash)
+    })
+})
+
+var hashedPassword = '$2a$10$kA1E0XwNoht5H4hZKjt0FOSNu9J7DvNU0rrjOvx3OD.EDEC4PH8xC'
+bcrypt.compare('123abc!', hashedPassword, (err, res)=>{
+    console.log(res)
+})
+
+// var data = {
+//     id: 10
+// }
+
+// //sign
+// var token = jwt.sign(data, '123abc')
+// console.log(token);
+
+// //verify
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded', decoded)
+
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`)
+
+// var data = {
+//     id: 4
+// }
+
+// var token ={
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString()
+
+// var resultHash = SHA256(JSON.stringify(token.data)+ 'somesecret').toString()
+
+// if(resultHash === token.hash){
+//     console.log('Data was not changed')
+// }else{
+//     console.log('Data was changed, Do not trust!')
+// }
